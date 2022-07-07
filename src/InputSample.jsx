7abbe9258a1546c2dcfd1ba50024749359abd685
BexFox <bexfox@BexFoxui-MacBookAir.local>
2022-07-07 14:12:27 +0900

    Complete UseRef

--- src/InputSample.jsx
+++ src/InputSample.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function InputSample() {
   const [inputs, setInputs] = useState({
     name: "",
     nickname: "",
   });
+  const nameInput = useRef(); // @@1
+  const nicknameInput = useRef(); // @@1-2
+
   const { name, nickname } = inputs;
 
   const onChange = (e) => {
@@ -21,16 +24,25 @@ function InputSample() {
       name: "",
       nickname: "",
     });
+    nicknameInput.current.focus(); //@@2
   };
 
   return (
     <div>
-      <input name="name" placeholder="name" onChange={onChange} value={name} />
+      <input
+        name="name"
+        placeholder="name"
+        onChange={onChange}
+        value={name}
+        ref={nameInput} //@@3
+        autoFocus
+      />
       <input
         name="nickname"
         placeholder="nickname"
         onChange={onChange}
         value={nickname}
+        ref={nicknameInput} //@@3-2
       />
       <button onClick={onReset}>Initialization</button>
       <div>
