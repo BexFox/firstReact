f92b86777edf52b163c93578389449a1cbd86f27
BexFox <bexfox@BexFoxui-MacBookAir.local>
2022-07-07 13:54:34 +0900

    Before useRef

--- src/InputSample.jsx
+++ src/InputSample.jsx
@@ -1,28 +1,44 @@
 import React, { useState } from "react";
 
 function InputSample() {
-  const [inputs, setInput] = useState({
+  const [inputs, setInputs] = useState({
     name: "",
     nickname: "",
   });
-
   const { name, nickname } = inputs;
 
   const onChange = (e) => {
-    setText(e.target.value);
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+      // #1 if [name]:'corea', which result will be?
+    });
   };
+
   const onReset = () => {
-    setText("");
+    setInputs({
+      name: "",
+      nickname: "",
+    });
   };
 
   return (
     <div>
-      <input placeholder="name" />
-      <input placeholder="nickname" />
+      <input name="name" placeholder="name" onChange={onChange} value={name} />
+      <input
+        name="nickname"
+        placeholder="nickname"
+        onChange={onChange}
+        value={nickname}
+      />
       <button onClick={onReset}>Initialization</button>
       <div>
-        <b>Value:</b>
-        name (nickname)
+        <b>Value: </b>
+        <div>
+          Name: {name} <br />
+          Nickname: ({nickname})
+        </div>
       </div>
     </div>
   );
