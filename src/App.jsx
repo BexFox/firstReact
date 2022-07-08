7ac94c4340023019cb45a162841886c705159bda
BexFox <karis601293@gmail.com>
2022-07-08 15:48:47 +0900

    Make an array as useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React, { useRef, useState } from "react";
-import UserList from "./UserList";
-import CreateUser from "./CreateUser";
-import "./App.css";
+import React, { useRef, useState } from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
+import './App.css';
 
 function App() {
   const [inputs, setInputs] = useState({
-    username: "",
-    email: "",
+    username: '',
+    email: '',
   });
   const { username, email } = inputs;
   const onChange = (e) => {
@@ -17,36 +17,42 @@ function App() {
     });
   };
 
-  const users = [
+  const [users, setUsers] = useState([
     {
       id: 1,
-      username: "velopert",
-      email: "public.velopert@gmail.com",
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
     },
     {
       id: 2,
-      username: "tester",
-      email: "tester@example.com",
+      username: 'tester',
+      email: 'tester@example.com',
     },
     {
       id: 3,
-      username: "liz",
-      email: "liz@example.com",
+      username: 'liz',
+      email: 'liz@example.com',
     },
-  ];
+  ]);
 
   const nextId = useRef(4);
   const onCreate = () => {
-    // Needed to be added
-    // ...
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers([...users, user]);
+
     setInputs({
-      username: "",
-      email: "",
+      username: '',
+      email: '',
     });
     nextId.current += 1;
   };
+
   return (
-    <div className="App">
+    <div className='App'>
       <CreateUser
         username={username}
         email={email}
