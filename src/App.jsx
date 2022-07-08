4b2d54b641e38afa104581890839ca308918dcb0
BexFox <karis601293@gmail.com>
2022-07-08 19:48:36 +0900

    Practice again the componenets

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,7 @@
-import React, { useRef, useState } from 'react';
-import UserList from './UserList';
+import React, { useState, useRef } from 'react';
 import CreateUser from './CreateUser';
-import './App.css';
+import UserList from './UserList';
 
-function App() {
-  const [inputs, setInputs] = useState({
-    username: '',
-    email: '',
-  });
-  const { username, email } = inputs;
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
-
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com',
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com',
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com',
-    },
-  ]);
-
-  const nextId = useRef(4);
-  const onCreate = () => {
-    const user = {
-      id: nextId.current,
-      username,
-      email,
-    };
-    setUsers([users.concat(user)]); // different point
-
-    setInputs({
-      username: '',
-      email: '',
-    });
-    nextId.current += 1;
-  };
-
-  return (
-    <div className='App'>
-      <CreateUser
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList users={users} />
-    </div>
-  );
-}
+function App() {}
 
 export default App;
