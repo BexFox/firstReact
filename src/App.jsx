3e76ad0c1c6c732f827c7f72135f625e47d01731
BexFox <karis601293@gmail.com>
2022-07-14 10:55:56 +0900

    Add list on CreateUser component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,58 @@
 import React, { useState, useRef } from 'react';
 import UserList from './UserList';
-import InputSample from './InputSample';
+import CreateUser from './CreateUser';
 import './App.css';
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+    },
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    // Logic which will be put later
+    // ...
+    setInputs({
+      username: '',
+      email: '',
+    });
+    nextId.current += 1;
+  };
+
   return (
     <div className='App'>
-      <UserList />
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
     </div>
   );
 }
